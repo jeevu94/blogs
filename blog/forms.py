7feeb538b039484a71905666399b5ea65f8f0e80
jeevu94 <jeevan.jeevu94@gmail.com>
2022-09-28 13:07:25 +0530

    added tamil lang related field and modified in templates

--- blog/forms.py
+++ blog/forms.py
@@ -6,15 +6,20 @@ from .models import Blog
 class BlogForm(AppModelForm):
     class Meta(AppModelForm.Meta):
         fields = [
-            "title",
             "cover_image",
+            "title",
             "description",
+            "ta_title",
+            "ta_description",
         ]
         model = Blog
 
     def __init__(self, *args, **kwargs):
         self.author = kwargs.pop("author")
         super().__init__(*args, **kwargs)
+        self.fields["cover_image"].label = "Cover Photo"
+        self.fields["ta_title"].label = "தலைப்பு"
+        self.fields["ta_description"].label = "விளக்கம்"
 
     def clean_title(self):
         title = self.cleaned_data["title"].strip()
